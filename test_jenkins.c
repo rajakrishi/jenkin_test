80fc356d894616e1f0cdfbcf8194b4b64af8b184
Rishikesh Kumar Rajak <rajakr@mdc-01-ch3-cust1.labs.hpe.com>
2020-07-15 01:14:15 -0700

    Changed for 10th time

--- test_jenkins.c
+++ test_jenkins.c
@@ -6,4 +6,5 @@ int main() {
 	printf("3. Hello World in Rishikesh in devel branch!!\n");
 	printf("4. Hello World in Rishikesh in devel branch!!\n");
 	printf("5. Hello World in Rishikesh in devel branch!!\n");
+	printf("6. Hello World in Rishikesh in devel branch!!\n");
 }
